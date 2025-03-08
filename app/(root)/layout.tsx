cd07cbdd3d2a632688eefa1349984d90975a3f37
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2025-03-09 00:31:42 +0200

    Install and configure Cleck. Create topbar component

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,41 @@
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A next.js Meta Threads Application",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
+
+// https://www.youtube.com/watch?v=O5cmLDVTgAs   00:30:00
